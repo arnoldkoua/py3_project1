354c0c3c0cfbb1a1c1d85c5a2fccff6d29a65d40
arnoldkoua <48394812+arnoldkoua@users.noreply.github.com>
2021-04-25 17:35:00 +0200

    Add files via upload

---
+++ python3_projet_01.py
@@ -0,0 +1,42 @@
+#Exo 1
+# Créer un objet de type chaîne de caractères
+chaine = 
+# Créer un objet de type nombre entier
+nombre_entier = 
+# Créer objet de type nombre décimal
+nombre_decimal = 
+# Créer un objet de type booléen
+
+#Exo 1 - Solution
+# Créer un objet de type chaîne de caractères
+chaine = "Hello world"
+# Créer un objet de type nombre entier
+nombre_entier = 10
+# Créer objet de type nombre décimal
+nombre_decimal = 9.5
+# Créer un objet de type booléen
+booleen = True
+
+#Exo 2
+print('Bonjour tout le monde !")
+print('Je m'appelle Patrick.')
+print("Je dis souvent "la vie est un long fleuve tranquille" quand on s'appelle Patrick.")
+print(Oui, je suis trÃ¨s philosophe.)
+
+#Exo 2 - Solution
+print("Bonjour tout le monde !")
+print("Je m'appelle Patrick.")
+print("Je dis souvent la vie est un long fleuve tranquille quand on s'appelle Patrick.")
+print("Oui, je suis très philosophe.")
+
+#Exo 3
+a = -2..5
+b = true
+c = falce
+d = Bonjour tout le monde
+      
+#Exo 3 - Solution
+a = -2.5
+b = True
+c = False
+d = "Bonjour tout le monde"
